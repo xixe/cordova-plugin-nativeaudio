ca4ca4575617f4a363ea5f360a1c6404e7bf41b8
xixe <xixedev@gmail.com>
2020-07-22 17:12:44 +0700

    Update NativeAudio.java
    Replace 'www' to 'public' in assetPath

--- src/android/NativeAudio.java
+++ src/android/NativeAudio.java
@@ -82,7 +82,7 @@ public class NativeAudio extends CordovaPlugin implements AudioManager.OnAudioFo
 					voices = data.getInt(3);
 				}
 
-				String fullPath = assetPath;
+				String fullPath = "public/".concat(assetPath);
 
 				Context ctx = cordova.getActivity().getApplicationContext();
 				AssetManager am = ctx.getResources().getAssets();
