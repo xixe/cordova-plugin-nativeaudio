aa42f1d6d50327a4a3be53399e5de18a5c0c5f84
xixe <xixedev@gmail.com>
2020-07-22 17:12:52 +0700

    Update NativeAudio.m
    Replace 'www' to 'public' in assetPath

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -84,7 +84,7 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
     [self.commandDelegate runInBackground:^{
         if (existingReference == nil) {
 
-            NSString* basePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"www"];
+            NSString* basePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"public"];
             NSString* path = [NSString stringWithFormat:@"%@", assetPath];
             NSString* pathFromWWW = [NSString stringWithFormat:@"%@/%@", basePath, assetPath];
 
